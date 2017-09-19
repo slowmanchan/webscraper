f5f21167eccadd0a3131ca052e671ab262fc687c
Chan <nchan@hq.medicalert.ca>
2017-09-19 17:01:49 -0400

    added letter indexs

--- index.js
+++ index.js
@@ -1,49 +1,74 @@
+var async = require('async');
 var request = require('request');
 var cheerio = require('cheerio');
-var count = 1100;
-var options = {
-	url: 'http://www.metacritic.com/browse/movies/title/dvd?page=' + count,
-	headers: {
-		'User-Agent': 'Mozilla Firefox'
-	}
-};
 
-var scores = [];
-var titles = [];
-var metaData = [];
-var scoresTitles = {};
-var page = true;
+var page = 0;
+var indexCount = 0;
+var pageExists = true;
+var indexExists = true;
 
-
-	request(options, function(err, res, html) {
-		if(err) { console.log(err) }
-
-		var $ = cheerio.load(html);
-		console.log('status code:' + res.statusCode);
-		console.log(res.headers);
-
-
-		$('td.score_wrapper div.metascore_w').each(function(i, node) {
-			scores.push($(this).text());
-		})
-
-		$('td.title_wrapper div.title a').each(function(i, node) {
-			titles.push($(this).text());
-		})
-
-		for (i = 0; i < scores.length; i++) {
-			scoresTitles = {
-				score: scores[i],
-				title: titles[i]
+async.whilst(
+    function(){
+		return (indexExists);
+	},
+	function(next){
+		var letter = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+		var options = {
+			url: 'http://www.metacritic.com/browse/movies/title/dvd/' + letter[indexCount] + '?page=' + page,
+			headers: {
+				'User-Agent': 'Mozilla Firefox'
 			}
-			metaData.push(scoresTitles);
-		}
-		//console.log(metaData);
-		if ($('div[class=pad_top1]').text().trim() == 'No movies found.') {
-			page = false;
-
-		}
+		};
+		var scores = [];
+		var titles = [];
+		var metaData = [];
+		var scoresTitles = {};
 		
-	})
-console.log(page)
-	count++;
+		
+		request(options, function(err, res, html) {
+			if(err) { console.log(err) }
+
+			var $ = cheerio.load(html);
+			console.log('status code:' + res.statusCode);
+			console.log(res.headers);
+
+
+			$('td.score_wrapper div.metascore_w').each(function(i, node) {
+				scores.push($(this).text());
+			})
+
+			$('td.title_wrapper div.title a').each(function(i, node) {
+				titles.push($(this).text());
+			})
+
+			for (i = 0; i < scores.length; i++) {
+				scoresTitles = {
+					score: scores[i],
+					title: titles[i]
+				}
+				metaData.push(scoresTitles);
+			}
+			
+			console.log(metaData);
+			
+			page++;
+			
+			if ($('div[class=pad_top1]').text().trim() == 'No movies found.') {
+				page = 0;
+				indexCount++;
+			}
+			
+			if (indexCount > 26) {
+				indexExists = false;
+			}
+		
+			next();
+		})
+       
+		
+	},
+	function(err) { console.log(err) }
+);
+	
+
+	
